353dd913fae23dcf9d139ccc42a3cc6763599c92
khat0076 <khattab2003@live.com>
2025-12-08 15:32:39 +0100

    Added the stuff from the uml, and changed some stuff from the uml

--- src/main/java/com/bankapp/BankMenu.java
+++ src/main/java/com/bankapp/BankMenu.java
@@ -1,12 +1,35 @@
 package com.bankapp;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BankMenu {
 
-
+    //Ui so we dont use println
     public UIText ui;
 
+    //
+    public User loggedInUser;
+
+    //Arraylist to hold the bankMenu options
+    private ArrayList<String> bankMenuOptions;
+
+
+    //Getter method
+    public ArrayList<String> getBankMenuOptions() {
+        return bankMenuOptions;
+    }
+
+
+    //Method to connect to the database
+    public void DBConnection(){
+        //A variable to hold the database URL
+        String databaseURL = ("jdbc:sqlite:identifier.sqlite");
+        //Create a new instance of DBConnection
+        DBconnection DBConnection = new DBconnection();
+        //Connect to the database via the url
+        DBConnection.connect(databaseURL);
+    }
 
 
 
@@ -28,7 +51,34 @@ public class BankMenu {
     }
 
 
-    //Method to showMainMenu()
+    //Method where we create the showMainMenu() options
+    public void showMainMenu(){
 
+    }
+
+    //Method where user chooses the option
+    public void chooseFromMenu(){
+
+    }
+
+    //
+    public void handleDeposit(){
+
+    }
+
+    //
+    public void handleWithdraw(){
+
+    }
+
+    //
+    public void showBalance(){
+
+    }
+
+    //
+    public void showTransactions(){
+
+    }
 
 }
