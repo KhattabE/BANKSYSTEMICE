87ea83ab2feed927410fac804a7e3f1022980415
khat0076 <khattab2003@live.com>
2025-12-09 10:47:35 +0100

    Changed the database functions in the dbconnection class a bit, and adjusted some stuff in the user class

--- src/main/java/com/bankapp/DBconnection.java
+++ src/main/java/com/bankapp/DBconnection.java
@@ -19,43 +19,84 @@ public class DBconnection {
     }
 
 
-    public void insertIntoDataCreateAccount(String userName, String userCode){
 
+
+
+    //Method to insert the information about the user when they create an account
+    public void createBankAccountInformation(int userId, String userName, String firstName, String lastName, String userMail, String userCode, String phoneNumber){
         String sql = """
-                INSERT INTO bankUser (user_name, user_password)
-                VALUES (?,?);
-                """;
+                    INSERT INTO bankUser(user_id, user_name, first_name, last_name, user_mail, user_password, phoneNumber)
+                    VALUES (?,?,?,?,?,?,?)
+                    """;
 
         try{
 
             //What this does is that it lets us insert data safely into the sql query
             PreparedStatement pstmt = connection.prepareStatement(sql);
 
-
             //We insertthe variables into the sql here
-            pstmt.setString(1, userName);
-            pstmt.setString(2, userCode);
-
-            pstmt.executeQuery();
+            pstmt.setInt(1, userId);
+            pstmt.setString(2, userName);
+            pstmt.setString(3, firstName);
+            pstmt.setString(4, lastName);
+            pstmt.setString(5, userMail);
+            pstmt.setString(6, userCode);
+            pstmt.setString(7, phoneNumber);
 
 
             //And here we execute the insert
+            pstmt.executeQuery();
+
+            //And here we execute the insert
             pstmt.executeUpdate();
+
 
             //Just feedback to check if it is working
             System.out.println("Information has been added successfully!");
 
 
-
         }catch (SQLException sqle){
             System.out.println("Data could not be inserted into the database");
         }
+    }
 
+    //Method to get the userInformation if needed
+    public void userInformation(){
+        String sql = """
+                SELECT user_id, user_name, first_name, last_name, user_mail, user_password, phoneNumber FROM bankUser
+                """;
+
+        try{
+            Statement stmt = connection.createStatement();
+            ResultSet set = stmt.executeQuery(sql);
+
+            int userID = set.getInt("user_id");
+            String userName = set.getString("user_name");
+            String firstName = set.getString("first_name");
+            String lastName = set.getString("last_name");
+            String userMail = set.getString("user_mail");
+            String userPassword = set.getString("user_password");
+            String userPhoneNumber = set.getString("phoneNumber");
+
+            System.out.println("User ID:" + " - " + userID +
+                    "\nUser Name:" + " - " + userName +
+                    "\nUser First Name:" + " - " + firstName +
+                    "\nUser Last Name:" + " - " + lastName +
+                    "\nUser Mail:" + " - " + userMail +
+                    "\nUser Password:" + " - " + userPassword +
+                    "\nUser Phone Number:" + " - " + userPhoneNumber);
+
+        }catch (SQLException sqle){
+            System.out.println("Could not retrive the data!");
+        }
 
     }
 
 
 
+
+
+    //Method to get the user login information from the database
     public void userLoginInformation(){
         String sql = """
                 SELECT user_name, user_password FROM bankUser
@@ -75,6 +116,15 @@ public class DBconnection {
         }
 
     }
+
+
+
+
+
+
+
+
+
 
 
 
--- src/main/java/com/bankapp/User.java
+++ src/main/java/com/bankapp/User.java
@@ -3,86 +3,74 @@ package com.bankapp;
 import java.util.List;
 
 public class User {
+
+    //Class fields
     private int userID;
-    private String foreName;
-    private String surName;
     private String userName;
+    private String foreName;
+    private String lastName;
+    private String userEmail;
     private int password;
-    private String email;
     private String phoneNumber;
     private List<Account> accounts;
 
-    public User(int userID, String foreName, String surName, String userName, int password, String email, String phoneNumber) {
+
+    //Class constructor
+    public User(int userID, String userName, String foreName, String lastName, String userEmail, int password, String phoneNumber) {
         this.userID = userID;
         this.foreName = foreName;
-        this.surName = surName;
+        this.lastName = lastName;
         this.userName = userName;
         this.password = password;
-        this.email = email;
+        this.userEmail = userEmail;
         this.phoneNumber = phoneNumber;
     }
 
+
+
+    //Getters
     public int getUserID() {
         return userID;
-    }
-
-    public void setUserID(int userID) {
-        this.userID = userID;
-    }
-
-    public String getForeName() {
-        return foreName;
-    }
-
-    public void setForeName(String foreName) {
-        this.foreName = foreName;
-    }
-
-    public String getSurName() {
-        return surName;
-    }
-
-    public void setSurName(String surName) {
-        this.surName = surName;
     }
 
     public String getUserName() {
         return userName;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public String getForeName() {
+        return foreName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
     }
 
     public int getPassword() {
         return password;
     }
 
-    public void setPassword(int password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPhoneNumber() {
         return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
     }
 
     public List<Account> getAccounts() {
         return accounts;
     }
 
-    public void setAccounts(List<Account> accounts) {
-        this.accounts = accounts;
+
+
+    //Method to check password
+    public boolean checkPassword(String password){
+
     }
+
+
+
+
+
 }
+
